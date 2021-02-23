7dfa30dec4cb3ee04613b76cc79193422a4d95b1
Richard-JungJin-Park <76423126+Richard-JungJin-Park@users.noreply.github.com>
2021-02-23 17:44:27 +0900

    my first commit

---
+++ R/header2lower.R
@@ -0,0 +1,12 @@
+#' h2l() Function
+#'
+#' This function converts header to lower case
+#' @param x :
+#' @keywords h2l
+#' @export
+#' @examples
+#' h2l(x)
+
+h2l <- function(x) {
+  names(x) <- tolower(names(x))
+}
